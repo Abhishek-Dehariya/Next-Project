21e284974793a91c9f89fb2f2b1646aa2d7ebb88
Abhishek Dehariya <abhihek.dehariya@terragi.com>
2024-10-02 20:33:18 +0530

    basic routing

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function Home() {
+  return (
+    <div>
+      <h6>This is about</h6>
+      <Link href="home">Go to Home Page</Link>
+    </div>
+  );
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function Home() {
+  return (
+    <div>
+      <h6>This is Login Page</h6>
+      <Link href="home">Go to Home Page</Link>
+    </div>
+  );
+}
