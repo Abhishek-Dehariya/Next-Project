a5312dc8e39ea85279089ec095e3dd00b1351b2b
Abhishek Dehariya <abhihek.dehariya@terragi.com>
2024-10-03 11:46:24 +0530

    New

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h6>This is about</h6>
-      <Link href="home">Go to Home Page</Link>
+      <h6>This is Login Page</h6>
+      <Link href="/">Go to Home Page</Link>
     </div>
   );
 }
